5ced64ec8e9eb9d8796a6ec165c6043518e9bebf
ericnoonan00 <70498751+ericnoonan00@users.noreply.github.com>
2020-11-29 15:25:15 -0500

    Add files via upload

---
+++ Lab 7/SharkObjects.py
@@ -0,0 +1,21 @@
+class Shark:
+    def __init__(self, name):
+        self.name = name
+
+    def swim(self):
+        print(self.name + " is swimming.")
+
+    def be_awesome(self):
+        print(self.name + " is being awesome.")
+
+def main():
+    sammy = Shark("Sammy")
+    sammy.be_awesome()
+    stevie = Shark("Stevie")
+    stevie.swim()
+    eric = Shark("Eric")
+    eric.swim()
+    eric.be_awesome()
+
+if __name__ == "__main__":
+  main()
